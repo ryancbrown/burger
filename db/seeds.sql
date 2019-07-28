48f6a5f920ef4dab81e66fe75fc06e23b397553a
rcbnyc <16548379+ryancbrown@users.noreply.github.com>
2019-07-28 16:06:40 -0400

    set up db and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Yummy', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Stacked', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('California', false);
